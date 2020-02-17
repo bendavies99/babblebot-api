877c9b311b0faa08c9e3c9cfab3a46cf46b9ce5f
Ben Davies <ben@bj-davies.co.uk>
2020-02-17 14:19:09 +0000

    style(prettier): setup prettier

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,7 @@
-import {add} from "./../src/index";
+import { add } from './../src/index';
 
-describe("Add Function", () => {
-  it("Should add 2 numbers together", () => {
+describe('Add Function', () => {
+  it('Should add 2 numbers together', () => {
     expect(add(1, 2)).toBe(3);
     expect(add(2, 5)).toBe(7);
   });
